cb54b30e8d4d736f62003c4c572b40e5ec67e84a
matousbalat <matous.balat@gmail.com>
2026-02-04 11:36:49 +0100

    3.3.2

---
+++ 3.3.2/script.js
@@ -0,0 +1,21 @@
+let userName = "matouš"
+console.log(userName)
+
+userName = "matýsek"
+console.log(userName)
+
+console.log(7+4)
+console.log("7"+4)
+console.log("7"+"4")
+console.log("SIX"+"SEVEN")
+
+let age = 67;
+console.log(age > 20);
+
+let text = "Text"
+console.log(typeof text)
+
+let a;
+console.log(a)
+let b = null;
+console.log(b)
