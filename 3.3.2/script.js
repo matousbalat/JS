c7c1f64ecbf26458c751b6d375f1ca4053c2027c
matousbalat <matous.balat@gmail.com>
2026-02-11 11:44:46 +0100

    3.3.2

--- 3.3.2/script.js
+++ 3.3.2/script.js
@@ -19,3 +19,39 @@ let a;
 console.log(a)
 let b = null;
 console.log(b)
+
+let isLoggedIn = false;
+console.log(typeof isLoggedIn)
+
+// aritmetické operace
+
+let cisloA = 1;
+let cisloB = 2;
+console.log(cisloA + cisloB)
+console.log(cisloA - cisloB)
+console.log(cisloA * cisloB)
+console.log(cisloA / cisloB)
+
+// umocňování **
+
+console.log(cisloA ** cisloB)
+
+// zbytek - modulo
+
+console.log(cisloA % cisloB);
+console.log(cisloA + cisloB * cisloB);
+console.log((cisloA + cisloB) * cisloB);
+
+// Aktualizace proměnné (přiřazovací operátory)
+
+let number = 18;
+number = number + 2;
+number += 1
+number *= 2
+number /= 2
+number -= 1
+number %= 3
+number ++;
+number --
+
+console.log(number)
---
+++ cviceni 3.3.2/script.js
@@ -0,0 +1,18 @@
+let userName = "matouš"
+let userName2 = "jája"
+
+console.log(userName)
+console.log(userName2)
+
+let age = 67;
+let age2 = 69;
+
+
+console.log(age)
+console.log(age2)
+
+let součet = age + age2
+console.log(součet)
+
+
+console.log(userName2 + userName + součet) 
