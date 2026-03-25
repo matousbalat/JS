940b0a3c7c47d2824b3803b9eb9b79fe1d9dda8a
matousbalat <matous.balat@gmail.com>
2026-03-25 11:28:40 +0100

    3.3.6

---
+++ 3.3.6/app.js
@@ -0,0 +1,28 @@
+let counter = 0;
+
+while (counter < 10) {
+    console.log(counter);
+    counter++;
+}
+
+let i = 1;
+
+while (i <= 10) {
+    let message = i
+
+     if (i === 7) {
+        message ="Číslo 7 je moje fav číslo.";
+    }
+
+    console.log(message);
+    i++;
+}
+    
+for (let a = 0; a < 10; a++) {
+    console.log(a);
+}
+
+for (let b = 0; b < 10; b++) {               
+    if (b === 7) break;
+    console.log(b);
+}
