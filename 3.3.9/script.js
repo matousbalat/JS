89d39c4833975860574e79a12928541acc99b1ec
matousbalat <matous.balat@gmail.com>
2026-05-20 11:32:47 +0200

    3.3.9

---
+++ 3.3.9/script.js
@@ -0,0 +1,22 @@
+console.log("It's a nice day")
+console.log('Moje jméno je "Batát"')
+
+let username = "Batát"
+let message = `Moje jméno je ${username}`;
+console.log(message);
+
+let a = 5;
+let b = 10;
+console.log(`Součet je ${a+b}`);
+
+function pozdrav(name) {
+    return`Hello, ${name}!`;
+}
+
+console.log(`${pozdrav('Batát')}`);
+
+let popis = `Toto je delší text,
+který se prostírá 
+přes několik řádků.`
+
+console.log(popis)
