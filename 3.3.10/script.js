223e4e771d7a5190f64c08f66b4c57abf319fbba
matousbalat <matous.balat@gmail.com>
2026-05-27 11:31:24 +0200

    3.3.10

---
+++ 3.3.10/script.js
@@ -0,0 +1,11 @@
+let myString = "Hello world!"
+let myBadString = "  Hello World   "
+
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.charAt(0));
+console.log(myString.indexOf("e"));
+console.log(myString.replace("H", "P"));
+console.log(myString.replaceAll("Hello world", "Ahoj světe"));
+console.log(myBadString.trim());
